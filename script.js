e2c42b64e8add72a20f83d5e067be3b45379e534
Vasek78951 <v3ndacalda@gmail.com>
2024-02-25 15:27:32 +0100

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function HideNav(color) {
+  var clrDiv = document.getElement("li")[0];
+  clrDiv.style.backgroundColor = color;
+  
+}
+
+document.getElementById("check").onclick = function() { ChangeColor("aqua"); }
